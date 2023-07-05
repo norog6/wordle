dd0ca267d96d1b17cb1244cfdb835c387dc9d114
norog6 <92385720+norog6@users.noreply.github.com>
2023-07-05 14:04:16 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,56 +1,49 @@
 import java.util.ArrayList;
 
 public class Main {
-    public static void wordle(ArrayList<Wordle> letters, String attempt,String word) {
+    public static Color[] wordle(String attempt, String word) {
+        Color color[]=new Color[5];
+        for (int i=0;i<=4;i++){
+            color[i]=Color.Black;
+        }
         if (word.equals(attempt)) {
             System.out.println("Correct");
-            return;
-        }
-        else {
+        } else {
             for (int i = 0; i <= 4; i++) {
-                if (letters.get(i).getLetter().equals(String.valueOf(word.charAt(i)))) {
-                    letters.get(i).setColor("green");
-                    letters.get(i).setLetter(".");
-                    word=word.substring(0,i)+"!"+word.substring(i+1,5);
-                   
+                if (String.valueOf(attempt.charAt(i)).equals(String.valueOf(word.charAt(i)))) {
+                    color[i] = Color.Green;
+                    attempt = attempt.substring(0, i) + "." + attempt.substring(i + 1, 5);
+                    word = word.substring(0, i) + "!" + word.substring(i + 1, 5);
+
                 }
             }
 
 
             for (int i = 0; i <= 4; i++) {
                 for (int j = 0; j <= 4; j++) {
-                    if (letters.get(i).getLetter().equals(String.valueOf(word.charAt(j)))) {
-                        letters.get(i).setColor("yellow");
-                        letters.get(i).setLetter(".");
-                        word=word.substring(0,i)+"!"+word.substring(i+1,5);
+                    if (String.valueOf(attempt.charAt(i)).equals(String.valueOf(word.charAt(j)))) {
+                        color[i] = Color.Yellow;
+                        attempt = attempt.substring(0, i) + "." + attempt.substring(i + 1, 5);
+                        word = word.substring(0, j) + "!" + word.substring(j + 1, 5);
                     }
                 }
             }
-            for (int i=0;i<=4;i++){
-                if(letters.get(i).getColor().equals("1")){
-                    letters.get(i).setColor("black");
-                }
-            }
-        }
-        for (int i=0;i<=4;i++)
-        {
-            System.out.println(letters.get(i).getColor()+" "+attempt.charAt(i));
-        }
-        
 
-        return;
+
+        }
+        return color;
     }
+
     public static void main(String[] args) {
-       String word="world";
-       String attempt="hello";
-        ArrayList<Wordle> letters=new ArrayList<Wordle>();
-       for (int i = 0; i<=4; i++)
-       {
-           Wordle letter=new Wordle(String.valueOf(attempt.charAt(i)),"1");
-           letters.add(letter);
-       }
-       wordle(letters,attempt,word);
+        String word = "agaaa";
+        String attempt = "aadag";
+        Color colors[]=wordle(attempt,word);
+        for (int i = 0; i <=4 ; i++) {
+            System.out.println(colors[i]);
+
+        }
+
     }
-    
-    
+
+
 }
